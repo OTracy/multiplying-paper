09d2560d4b26bd30d8e723c036998f795b929536
Tracy <tracelots95.3@gmail.com>
2018-02-23 18:25:44 +0000

    project deploy

---
+++ mp.js
@@ -0,0 +1,133 @@
+console.log('hello world')
+
+
+let white = document.querySelector('#white')
+let grid = document.querySelector('#grid')
+white.addEventListener("click", e => {
+
+    let image = document.createElement('img')
+    image.setAttribute("src", "https://cdn.shopify.com/s/files/1/0882/1686/products/solid_white_photo_studio_muslin_backdrop.jpg?v=1490271177")
+    // image.style.position = "absolute"
+    // image.setAttribute("height", "10px")
+    grid.appendChild(image)
+})
+
+
+////////NAVY///////////
+let navy = document.querySelector('#navy')
+navy.addEventListener("click", e => {
+    let image1 = document.createElement('img')
+    image1.style.position = "absolute"
+   image1.style.bottom="250px"
+   image1.style.left="500px"
+    image1.setAttribute("src", "https://www.cabinetmakerwarehouse.com/wp-content/uploads/969-navy-blue.jpg")
+    grid.appendChild(image1)
+    
+    let image2 = document.createElement('img')
+    image2.style.position = "absolute"
+   image2.style.bottom="150px"
+   image2.style.left="200px"
+    image2.setAttribute("src", "http://www.wampstore.com/store/image/cache/data/Wamp/Products/Vallejo/German%20cam%20dark%20Green-900x900.jpg")
+    grid.appendChild(image2)
+})
+///////////ORANGE////
+let orange = document.querySelector('#orange')
+orange.addEventListener("click", e => {
+    let image = document.createElement('img')
+    image.setAttribute("src","https://vignette.wikia.nocookie.net/symbolism/images/4/43/Orange.png/revision/latest?cb=20140818120046")
+    grid.appendChild(image)
+})
+    
+    //////REDDDD///
+let red = document.querySelector('#red')
+red.addEventListener("click", e => {
+    let image = document.createElement('img')
+    image.setAttribute("src", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtXDmgaa73nXcWGpsBJdbeQHJyM8CAfmYvUO0MrmL-Aq78B5Ai")
+    grid.appendChild(image)
+})
+    
+    
+    ///////uyufehjblf;lbfhe
+
+
+// let orange = document.querySelector('#orange')
+// orange.addEventListener("click", e => {
+//   let grid = document.querySelector('#grid')
+//   let image = document.createElement('img')
+//   image.style.postition = "absolute"
+//   image.style.bottom="200px"
+//   image.style.left="150px"
+//   image.setAttribute("src", "https://images.utrechtart.com/products/optionRegular/Jacquard/jacquard-Fluor-Pink_X.jpg
+// https://images.utrechtart.com/products/optionRegular/Jacquard/jacquard-Fluor-Pink_X.jpg
+// ")
+//   //image.setAttribute("height", "10px")
+//   grid.appendChild(image)
+// });
+
+// let red =document.querySelector('#red')
+// red.addEventListener("click", e => {
+//   let grid =document.querySelector('#grid')
+//   let image = document.createElement('img')
+//   image.style.position = "absolute"
+//   image.style.bottom="150px"
+//   image.style.left="200px"
+//   image.setAttribute("src", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSafXAKCsOfEaPfUg-Tsbdxe6eYb8DN_dY_nwX6KMef_LZAjEZczA
+// https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSafXAKCsOfEaPfUg-Tsbdxe6eYb8DN_dY_nwX6KMef_LZAjEZczA
+// ")
+//   //image.setAttribute("height", "10px")
+//   grid.appendChild(image)
+// });
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+//     var white = document.white('#white');
+// if(white && white.style) {
+//     white.style.height = '100px';
+//   white.style.width = '200px';
+// }
+    
+    
+    
+    
+// let red=document.querySelector("#red")
+// red.addEventListener("click", e => {
+// let grid=document.querySelector('#grid')
+// }
+
+
+
+
+
+
+
+
+//grab the image
+//add event listener
+//make changes
+
+
+//1. create an image- document.createElement('img')
+//2. set the source attribute src / set source = to whatever link wen is using to mkae white paper{ let image = document.c
+//3.let grid=document.querySelector
+//let imahe=document.createElement ('img')
+
+//atribute would go inside image
